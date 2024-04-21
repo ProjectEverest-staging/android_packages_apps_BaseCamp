ProjectEverest-wip/BaseCamp

BaseCamp: Add BatteryBar customizations [2/2]
Change-Id: I6ffcb245db5479e8d818cc31ceb573e17e38f0bb
Signed-off-by: Shubham Singh <coolsks94@gmail.com>
Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

src/com/base/camp/fragments/BatteryBar.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2018 AospExtended ROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.base.camp.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class BatteryBar extends SettingsPreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_BATT_BAR = "statusbar_battery_bar";
+
+    private SwitchPreference mBatteryBar;
+
+    private boolean mIsBarSwitchingMode = false;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.battery_bar);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+        int intColor;
+        String hexColor;
+
+        mBatteryBar = (SwitchPreference) findPreference(PREF_BATT_BAR);
+        mHandler = new Handler();
+
+        boolean showing = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUSBAR_BATTERY_BAR, 0, UserHandle.USER_CURRENT) != 0;
+        mBatteryBar.setChecked(showing);
+        mBatteryBar.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryBar) {
+            if (mIsBarSwitchingMode) {
+                return false;
+            }
+            mIsBarSwitchingMode = true;
+            boolean value = ((Boolean)newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.STATUSBAR_BATTERY_BAR,
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsBarSwitchingMode = false;
+                    boolean showing = Settings.System.getIntForUser(resolver,
+                            Settings.System.STATUSBAR_BATTERY_BAR, 0, UserHandle.USER_CURRENT) != 0;
+                    mBatteryBar.setChecked(showing);
+                }
+            }, 1500);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVEREST;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.battery_bar);
+}

ProjectEverest-staging/android_packages_apps_BaseCamp

src/com/everest/fragments/BatteryBar.java
@@ -1,0 +1,102 @@
+/*
+ * Copyright (C) 2018 AospExtended ROM Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.UserHandle;
+import android.provider.SearchIndexableResource;
+import android.provider.Settings;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreferenceCompat;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settingslib.search.SearchIndexable;
+
+@SearchIndexable
+public class BatteryBar extends SettingsPreferenceFragment
+            implements Preference.OnPreferenceChangeListener {
+
+    private static final String PREF_BATT_BAR = "statusbar_battery_bar";
+
+    private SwitchPreferenceCompat mBatteryBar;
+
+    private boolean mIsBarSwitchingMode = false;
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.battery_bar);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        PreferenceScreen prefSet = getPreferenceScreen();
+        int intColor;
+        String hexColor;
+
+        mBatteryBar = (SwitchPreferenceCompat) findPreference(PREF_BATT_BAR);
+        mHandler = new Handler();
+
+        boolean showing = Settings.System.getIntForUser(resolver,
+                Settings.System.STATUSBAR_BATTERY_BAR, 0, UserHandle.USER_CURRENT) != 0;
+        mBatteryBar.setChecked(showing);
+        mBatteryBar.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBatteryBar) {
+            if (mIsBarSwitchingMode) {
+                return false;
+            }
+            mIsBarSwitchingMode = true;
+            boolean value = ((Boolean)newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.STATUSBAR_BATTERY_BAR,
+                    value ? 1 : 0, UserHandle.USER_CURRENT);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mIsBarSwitchingMode = false;
+                    boolean showing = Settings.System.getIntForUser(resolver,
+                            Settings.System.STATUSBAR_BATTERY_BAR, 0, UserHandle.USER_CURRENT) != 0;
+                    mBatteryBar.setChecked(showing);
+                }
+            }, 1500);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.EVEREST;
+    }
+
+    public static final SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider(R.xml.battery_bar);
+}