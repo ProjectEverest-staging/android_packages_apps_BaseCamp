ProjectEverest-wip/BaseCamp

BaseCamp: move double line clock option to Base Camp [2/2]

src/com/base/camp/categories/LockScreenSettings.java
@@ -38,6 +38,8 @@ import android.provider.Settings;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 
+import com.everest.support.preferences.SecureSettingSwitchPreference;
+
 public class LockScreenSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
         
@@ -46,6 +48,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     private static final String BLUR_RADIUS_KEY = "ls_media_filter_blur_radius";
 
     Preference mBlurRadiusPref;
+
+     private static final String LOCKSCREEN_DOUBLE_LINE_CLOCK = "lockscreen_double_line_clock_switch";
+
+    	private SwitchPreference mKGCustomClockColor;
+    	private SecureSettingSwitchPreference mDoubleLineClock;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -61,6 +68,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
                 TextUtils.isEmpty(getResources().getString(com.android.internal.R.string.config_dozeTapSensorType))) {
             prefScreen.removePreference(ambientCat);
         }
+
+        mDoubleLineClock = (SecureSettingSwitchPreference ) findPreference(LOCKSCREEN_DOUBLE_LINE_CLOCK);
+        mDoubleLineClock.setChecked((Settings.Secure.getInt(getContentResolver(),
+             Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, 1) != 0));
+        mDoubleLineClock.setOnPreferenceChangeListener(this);
 
         mBlurRadiusPref = (Preference) findPreference(
                         BLUR_RADIUS_KEY);
@@ -79,6 +91,12 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
 
+            if (preference == mDoubleLineClock) {
+        	boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, value ? 1 : 0);
+            return true; 
+        }
         return false;
     }
 

ProjectEverest-staging/android_packages_apps_BaseCamp

src/com/everest/basecamp/categories/LockScreenSettings.java
@@ -42,6 +42,8 @@ import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settingslib.search.Indexable;
 import com.android.settingslib.search.SearchIndexable;
 
+import com.everest.support.preferences.SecureSettingSwitchPreference;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -51,6 +53,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment
             implements Preference.OnPreferenceChangeListener {
 
      private static final String CATEGORY_AMBIENT = "ambient_display";
+
+     private static final String LOCKSCREEN_DOUBLE_LINE_CLOCK = "lockscreen_double_line_clock_switch";
+
+    	private SwitchPreferenceCompat mKGCustomClockColor;
+    	private SecureSettingSwitchPreference mDoubleLineClock;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -65,10 +72,24 @@ public class LockScreenSettings extends SettingsPreferenceFragment
                 TextUtils.isEmpty(getResources().getString(com.android.internal.R.string.config_dozeTapSensorType))) {
             prefScreen.removePreference(ambientCat);
         }
+
+        mDoubleLineClock = (SecureSettingSwitchPreference ) findPreference(LOCKSCREEN_DOUBLE_LINE_CLOCK);
+        mDoubleLineClock.setChecked((Settings.Secure.getInt(getContentResolver(),
+             Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, 1) != 0));
+        mDoubleLineClock.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+
+            if (preference == mDoubleLineClock) {
+        	boolean value = (Boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.LOCKSCREEN_USE_DOUBLE_LINE_CLOCK, value ? 1 : 0);
+            return true; 
+        }
         return false;
     }  
 