ProjectEverest/packages_apps_BaseCamp

BaseCamp: Implement new font picker
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/base/camp/fragments/themes/fonts/FontArrayAdapter.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.base.camp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FontArrayAdapter extends ArrayAdapter<String> {
+    private SparseArray<Typeface> typefaceCache = new SparseArray<>();
+    private FontManager fontManager;
+    private List<String> fontPackageNames;
+    private List<Typeface> typefaces;
+    private Context mContext;
+
+    public FontArrayAdapter(Context context, int textViewResourceId, List<String> objects, FontManager fontManager) {
+        super(context, textViewResourceId, objects);
+        this.mContext = context;
+        this.fontPackageNames = objects;
+        this.fontManager = fontManager;
+        this.typefaces = fontManager.getFonts();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView view = (TextView) super.getView(position, convertView, parent);
+        Typeface typeface = getTypefaceForPosition(position);
+        if (typeface != null) {
+            view.setTypeface(typeface);
+        }
+        view.setText(getLabelForPosition(position));
+        return view;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView view = (TextView) super.getDropDownView(position, convertView, parent);
+        Typeface typeface = getTypefaceForPosition(position);
+        if (typeface != null) {
+            view.setTypeface(typeface);
+        }
+        view.setText(getLabelForPosition(position));
+        return view;
+    }
+
+    private String getLabelForPosition(int position) {
+        String packageName = fontPackageNames.get(position);
+        return fontManager.getLabel(mContext, packageName);
+    }
+
+    private Typeface getTypefaceForPosition(int position) {
+        Typeface typeface = typefaceCache.get(position);
+        if (typeface == null && position < typefaces.size()) {
+            typeface = typefaces.get(position);
+            typefaceCache.put(position, typeface);
+        }
+        return typeface;
+    }
+}

src/com/base/camp/fragments/themes/fonts/FontManager.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.base.camp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.om.OverlayInfo;
+import android.graphics.Typeface;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.android.internal.util.everest.ThemeUtils;
+
+public class FontManager {
+
+    private ThemeUtils mThemeUtils;
+
+    public FontManager(Context context) {
+        mThemeUtils = new ThemeUtils(context);
+    }
+
+    /**
+     * Get all available fonts and return as a list of typefaces.
+     */
+    public List<Typeface> getFonts() {
+        return mThemeUtils.getFonts();
+    }
+
+    /**
+     * Get all available font packages.
+     */
+    public List<String> getAllFontPackages() {
+        return mThemeUtils.getOverlayPackagesForCategory("android.theme.customization.font", "android");
+    }
+
+    /**
+     * Get the currently selected font package.
+     */
+    public String getCurrentFontPackage() {
+        List<OverlayInfo> overlayInfos = mThemeUtils.getOverlayInfos("android.theme.customization.font");
+        return overlayInfos.stream()
+                .filter(OverlayInfo::isEnabled)
+                .map(OverlayInfo::getPackageName)
+                .findFirst()
+                .orElse("android");
+    }
+
+    /**
+     * Enable a selected font package.
+     */
+    public void enableFontPackage(int position) {
+        if (position < 0 || position >= getAllFontPackages().size()) {
+            throw new IllegalArgumentException("Invalid font package position: " + position);
+        }
+        String selectedPackage = getAllFontPackages().get(position);
+        mThemeUtils.setOverlayEnabled("android.theme.customization.font", selectedPackage, "android");
+    }
+
+    /**
+     * Gets the font package label. NOTE: we use the actual font family alias for label
+     */
+    public String getLabel(Context context, String pkg) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            return pm.getApplicationInfo(pkg, 0).loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {}
+        return pkg;
+    }
+}
+

src/com/base/camp/fragments/themes/fonts/FontPickerPreview.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.base.camp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.text.SpannableString;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
+import android.util.TypedValue;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.base.camp.fragments.themes.fonts.FontManager;
+import com.base.camp.fragments.themes.fonts.FontArrayAdapter;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
+import java.util.List;
+
+public class FontPickerPreview extends SettingsPreferenceFragment {
+
+    private Spinner fontSpinner;
+    private TextView previewText;
+    private FontManager fontManager;
+    private ExtendedFloatingActionButton applyFab;
+    private int currentFontPosition = -1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fontManager = new FontManager(getActivity());
+        getActivity().setTitle(getActivity().getString(R.string.font_styles_title));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.font_picker_preview, container, false);
+        fontSpinner = rootView.findViewById(R.id.font_spinner);
+        previewText = rootView.findViewById(R.id.font_preview_text);
+        String text = previewText.getText().toString();
+        SpannableString spannableString = new SpannableString(text);
+        TypedValue typedValue = new TypedValue();
+        getActivity().getTheme().resolveAttribute(android.R.attr.colorAccent, typedValue, true);
+        int colorAccent = typedValue.data;
+        int startIndex = text.indexOf("A");
+        int endIndex = text.length();
+        spannableString.setSpan(
+            new ForegroundColorSpan(colorAccent), 
+            startIndex, 
+            endIndex, 
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        previewText.setText(spannableString);
+        List<String> fontPackageNames = fontManager.getAllFontPackages();
+        FontArrayAdapter fontAdapter = new FontArrayAdapter(
+            getActivity(),
+            android.R.layout.simple_spinner_dropdown_item,
+            fontPackageNames,
+            fontManager
+        );
+        fontSpinner.setAdapter(fontAdapter);
+        String currentFontPackage = fontManager.getCurrentFontPackage();
+        currentFontPosition = fontPackageNames.indexOf(currentFontPackage);
+        if (currentFontPosition != -1) {
+            fontSpinner.setSelection(currentFontPosition);
+        }
+        fontSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentFontPosition = position;
+                applyFontToPreview(fontPackageNames.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        applyFab = rootView.findViewById(R.id.apply_extended_fab);
+        applyFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fontManager.enableFontPackage(currentFontPosition);
+            }
+        });
+        return rootView;
+    }
+
+    private void applyFontToPreview(String fontPackage) {
+        String fontFamilyLabel = fontManager.getLabel(getContext(), fontPackage).toLowerCase();
+        Typeface typeface = Typeface.create(fontFamilyLabel, Typeface.NORMAL);
+        if (typeface != null && !fontFamilyLabel.equals("googlesans")) {
+            previewText.setTypeface(typeface);
+        } else {
+            previewText.setTypeface(Typeface.create("googlesans", Typeface.NORMAL));
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVEREST;
+    }
+}

ProjectEverest-staging/android_packages_apps_BaseCamp

src/com/everest/basecamp/fragments/themes/fonts/FontArrayAdapter.java
@@ -1,0 +1,78 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class FontArrayAdapter extends ArrayAdapter<String> {
+    private SparseArray<Typeface> typefaceCache = new SparseArray<>();
+    private FontManager fontManager;
+    private List<String> fontPackageNames;
+    private List<Typeface> typefaces;
+    private Context mContext;
+
+    public FontArrayAdapter(Context context, int textViewResourceId, List<String> objects, FontManager fontManager) {
+        super(context, textViewResourceId, objects);
+        this.mContext = context;
+        this.fontPackageNames = objects;
+        this.fontManager = fontManager;
+        this.typefaces = fontManager.getFonts();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        TextView view = (TextView) super.getView(position, convertView, parent);
+        Typeface typeface = getTypefaceForPosition(position);
+        if (typeface != null) {
+            view.setTypeface(typeface);
+        }
+        view.setText(getLabelForPosition(position));
+        return view;
+    }
+
+    @Override
+    public View getDropDownView(int position, View convertView, ViewGroup parent) {
+        TextView view = (TextView) super.getDropDownView(position, convertView, parent);
+        Typeface typeface = getTypefaceForPosition(position);
+        if (typeface != null) {
+            view.setTypeface(typeface);
+        }
+        view.setText(getLabelForPosition(position));
+        return view;
+    }
+
+    private String getLabelForPosition(int position) {
+        String packageName = fontPackageNames.get(position);
+        return fontManager.getLabel(mContext, packageName);
+    }
+
+    private Typeface getTypefaceForPosition(int position) {
+        Typeface typeface = typefaceCache.get(position);
+        if (typeface == null && position < typefaces.size()) {
+            typeface = typefaces.get(position);
+            typefaceCache.put(position, typeface);
+        }
+        return typeface;
+    }
+}

src/com/everest/basecamp/fragments/themes/fonts/FontManager.java
@@ -1,0 +1,84 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.content.om.OverlayInfo;
+import android.graphics.Typeface;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.android.internal.util.everest.ThemeUtils;
+
+public class FontManager {
+
+    private ThemeUtils mThemeUtils;
+
+    public FontManager(Context context) {
+        mThemeUtils = new ThemeUtils(context);
+    }
+
+    /**
+     * Get all available fonts and return as a list of typefaces.
+     */
+    public List<Typeface> getFonts() {
+        return mThemeUtils.getFonts();
+    }
+
+    /**
+     * Get all available font packages.
+     */
+    public List<String> getAllFontPackages() {
+        return mThemeUtils.getOverlayPackagesForCategory("android.theme.customization.font", "android");
+    }
+
+    /**
+     * Get the currently selected font package.
+     */
+    public String getCurrentFontPackage() {
+        List<OverlayInfo> overlayInfos = mThemeUtils.getOverlayInfos("android.theme.customization.font");
+        return overlayInfos.stream()
+                .filter(OverlayInfo::isEnabled)
+                .map(OverlayInfo::getPackageName)
+                .findFirst()
+                .orElse("android");
+    }
+
+    /**
+     * Enable a selected font package.
+     */
+    public void enableFontPackage(int position) {
+        if (position < 0 || position >= getAllFontPackages().size()) {
+            throw new IllegalArgumentException("Invalid font package position: " + position);
+        }
+        String selectedPackage = getAllFontPackages().get(position);
+        mThemeUtils.setOverlayEnabled("android.theme.customization.font", selectedPackage, "android");
+    }
+
+    /**
+     * Gets the font package label. NOTE: we use the actual font family alias for label
+     */
+    public String getLabel(Context context, String pkg) {
+        PackageManager pm = context.getPackageManager();
+        try {
+            return pm.getApplicationInfo(pkg, 0).loadLabel(pm).toString();
+        } catch (PackageManager.NameNotFoundException e) {}
+        return pkg;
+    }
+}
+

src/com/everest/basecamp/fragments/themes/fonts/FontPickerPreview.java
@@ -1,0 +1,128 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.everest.basecamp.fragments.themes.fonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.text.SpannableString;
+import android.text.Spannable;
+import android.text.style.ForegroundColorSpan;
+import android.util.TypedValue;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.everest.basecamp.fragments.themes.fonts.FontManager;
+import com.everest.basecamp.fragments.themes.fonts.FontArrayAdapter;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+
+import java.util.List;
+
+public class FontPickerPreview extends SettingsPreferenceFragment {
+
+    private Spinner fontSpinner;
+    private TextView previewText;
+    private FontManager fontManager;
+    private ExtendedFloatingActionButton applyFab;
+    private int currentFontPosition = -1;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        fontManager = new FontManager(getActivity());
+        getActivity().setTitle(getActivity().getString(R.string.font_styles_title));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.font_picker_preview, container, false);
+        fontSpinner = rootView.findViewById(R.id.font_spinner);
+        previewText = rootView.findViewById(R.id.font_preview_text);
+        String text = previewText.getText().toString();
+        SpannableString spannableString = new SpannableString(text);
+        TypedValue typedValue = new TypedValue();
+        getActivity().getTheme().resolveAttribute(android.R.attr.colorAccent, typedValue, true);
+        int colorAccent = typedValue.data;
+        int startIndex = text.indexOf("A");
+        int endIndex = text.length();
+        spannableString.setSpan(
+            new ForegroundColorSpan(colorAccent), 
+            startIndex, 
+            endIndex, 
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        );
+        previewText.setText(spannableString);
+        List<String> fontPackageNames = fontManager.getAllFontPackages();
+        FontArrayAdapter fontAdapter = new FontArrayAdapter(
+            getActivity(),
+            android.R.layout.simple_spinner_dropdown_item,
+            fontPackageNames,
+            fontManager
+        );
+        fontSpinner.setAdapter(fontAdapter);
+        String currentFontPackage = fontManager.getCurrentFontPackage();
+        currentFontPosition = fontPackageNames.indexOf(currentFontPackage);
+        if (currentFontPosition != -1) {
+            fontSpinner.setSelection(currentFontPosition);
+        }
+        fontSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                currentFontPosition = position;
+                applyFontToPreview(fontPackageNames.get(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+        applyFab = rootView.findViewById(R.id.apply_extended_fab);
+        applyFab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fontManager.enableFontPackage(currentFontPosition);
+            }
+        });
+        return rootView;
+    }
+
+    private void applyFontToPreview(String fontPackage) {
+        String fontFamilyLabel = fontManager.getLabel(getContext(), fontPackage).toLowerCase();
+        Typeface typeface = Typeface.create(fontFamilyLabel, Typeface.NORMAL);
+        if (typeface != null && !fontFamilyLabel.equals("googlesans")) {
+            previewText.setTypeface(typeface);
+        } else {
+            previewText.setTypeface(Typeface.create("googlesans", Typeface.NORMAL));
+        }
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.EVEREST;
+    }
+}